89985c8d3449d9aaeee77671d01136f81066299c
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-11-16 22:28:27 +0100

    routing done

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo "Hello there 👋";
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'],'/');
+$path = parse_url($path,PHP_URL_PATH);
+
+Routing::get('index','DefaultController');
+Routing::get('register','DefaultController');
+Routing::get('sign_in','DefaultController');
+Routing::get('booking','DefaultController');
+Routing::get('contact','DefaultController');
+Routing::get('discount','DefaultController');
+Routing::get('f40','DefaultController');
+Routing::get('main_page','DefaultController');
+Routing::get('our_cars','DefaultController');
+Routing::get('porsche_tycan','DefaultController');
+Routing::get('register_data_input','DefaultController');
+Routing::run($path);
--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -1,6 +1,67 @@
 <?php
 
-class DefaultController
+require_once 'AppController.php';
+
+class DefaultController extends AppController
 {
+    public function index()
+    {
+        $this->render('register');
+    }
     
+    public function register()
+    {
+        $this->render('register');
+    }
+
+    public function sign_in()
+    {
+        $this->render('sign_in');
+
+    }
+    
+    public function booking()
+    {
+        $this->render('booking');
+    }
+    
+    public function contact()
+    {
+        $this->render('contact');
+    }
+
+    public function discount()
+    {
+        $this->render('discount');
+    }
+
+    public function f40()
+    {
+        $this->render('f40');
+    }
+
+    public function faq()
+    {
+        $this->render('faq');
+    }
+
+    public function main_page()
+    {
+        $this->render('main-page');
+    }
+
+    public function our_cars()
+    {
+        $this->render('our_cars');
+    }
+
+    public function porsche_tycan()
+    {
+        $this->render('porsche_tycan');
+    }
+
+    public function register_data_input()
+    {
+        $this->render('register_data_input');
+    }
 }
