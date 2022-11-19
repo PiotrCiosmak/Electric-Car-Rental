459e381178fc9042f9580dbe8211460d1914ccb2
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-11-19 15:57:33 +0100

    removed couple filed

--- src/models/User.php
+++ src/models/User.php
@@ -4,27 +4,12 @@ class User
 {
     private $email;
     private $password;
-    private $firstName;
-    private $LastName;
-    private $phoneNumber;
-    private $street;
-    private $houseNumber;
-    private $apartmentNumber;
-    private $postCode;
-    private $town;
 
-    public function __construct(string $email, string $password, string $firstName, string $LastName, string $phoneNumber, string $street, string $houseNumber, string $apartmentNumber, string $postCode, string $town)
+
+    public function __construct(string $email, string $password)
     {
         $this->email = $email;
         $this->password = $password;
-        $this->firstName = $firstName;
-        $this->LastName = $LastName;
-        $this->phoneNumber = $phoneNumber;
-        $this->street = $street;
-        $this->houseNumber = $houseNumber;
-        $this->apartmentNumber = $apartmentNumber;
-        $this->postCode = $postCode;
-        $this->town = $town;
     }
 
     public function getEmail(): string
@@ -46,86 +31,4 @@ class User
     {
         $this->password = $password;
     }
-
-    public function getFirstName(): string
-    {
-        return $this->firstName;
-    }
-
-    public function setFirstName(string $firstName)
-    {
-        $this->firstName = $firstName;
-    }
-
-    public function getLastName(): string
-    {
-        return $this->LastName;
-    }
-
-    public function setLastName(string $LastName)
-    {
-        $this->LastName = $LastName;
-    }
-
-    public function getPhoneNumber(): string
-    {
-        return $this->phoneNumber;
-    }
-
-    public function setPhoneNumber(string $phoneNumber)
-    {
-        $this->phoneNumber = $phoneNumber;
-    }
-
-    public function getStreet(): string
-    {
-        return $this->street;
-    }
-
-    public function setStreet(string $street)
-    {
-        $this->street = $street;
-    }
-
-    public function getHouseNumber(): string
-    {
-        return $this->houseNumber;
-    }
-
-    public function setHouseNumber(string $houseNumber)
-    {
-        $this->houseNumber = $houseNumber;
-    }
-
-    public function getApartmentNumber(): string
-    {
-        return $this->apartmentNumber;
-    }
-
-    public function setApartmentNumber(string $apartmentNumber)
-    {
-        $this->apartmentNumber = $apartmentNumber;
-    }
-
-    public function getPostCode(): string
-    {
-        return $this->postCode;
-    }
-
-    public function setPostCode(string $postCode)
-    {
-        $this->postCode = $postCode;
-    }
-
-    public function getTown(): string
-    {
-        return $this->town;
-    }
-
-    public function setTown(string $town)
-    {
-        $this->town = $town;
-    }
-
-
 }
