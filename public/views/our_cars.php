765112fcb3058928e8e964b79a356d087c27b2e8
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-11-24 14:59:59 +0100

    our cars bug fix

--- public/views/our_cars.php
+++ public/views/our_cars.php
@@ -6,177 +6,173 @@
     </style>
     <script src="https://kit.fontawesome.com/8d0d2d9a42.js" crossorigin="anonymous"></script>
     <title>NASZE AUTA</title>
-    
+
 </head>
 
 <body>
-    <div class="our-cars-container">
-        <nav>
-            <ul>
-                <li>
-                    <a href="main_page" class="menu-button">GŁÓWNA</a>
-                </li>
-                <li>
-                    <a href="our_cars" class="menu-button">NASZE AUTA</a>
-                </li>
-                <li>
-                    <a href="f40" class="menu-button">F40 NA WESELE</a>
-                </li>
-                <li>
-                    <a href="faq" class="menu-button">FAQ</a>
-                </li>
-                <li>
-                    <a href="discount" class="menu-button">RABAT</a>
-                </li>
-                <li>
-                    <a href="contact" class="menu-button">KONTAKT</a>
-                </li>
-            </ul>
-            <a href="sign_in" class="logout-button">
-                <img src="public/img/logout.svg" id="logout">
-            </a>
-        </nav>
-        <main class="our-cars-main">
-            <div class="title">
-                <h2>NASZE AUTA</h2>
+<div class="our-cars-container">
+    <nav>
+        <ul>
+            <li>
+                <a href="main_page" class="menu-button">GŁÓWNA</a>
+            </li>
+            <li>
+                <a href="our_cars" class="menu-button">NASZE AUTA</a>
+            </li>
+            <li>
+                <a href="f40" class="menu-button">F40 NA WESELE</a>
+            </li>
+            <li>
+                <a href="faq" class="menu-button">FAQ</a>
+            </li>
+            <li>
+                <a href="discount" class="menu-button">RABAT</a>
+            </li>
+            <li>
+                <a href="contact" class="menu-button">KONTAKT</a>
+            </li>
+        </ul>
+        <a href="sign_in" class="logout-button">
+            <img src="public/img/logout.svg" id="logout">
+        </a>
+    </nav>
+    <main class="our-cars-main">
+        <div class="title">
+            <h2>NASZE AUTA</h2>
+        </div>
+        <header>
+            <div>
+                <form>
+                    <input id="input-search-bar" placeholder="wyszukaj">
+                </form>
             </div>
-            <header>
-                <div>
-                    <form>
-                        <input id="input-search-bar" placeholder="wyszukaj">
-                   </form>
+        </header>
+
+        <div class="cars">
+            <a href="porsche_tycan">
+                <div class="car">
+                    <img src="public/img/porche.webp">
+                    <div class="background-of-car">
+                        <p>Porsche Tycan</p>
+                        <img src="public/img/0-100icon.svg">
+                        <p>2,6</p>
+                    </div>
                 </div>
-            </header>
-            <div class="row-of-cars">
-                <a href="porsche_tycan">
-                    <div class="car">
-                        <img src="public/img/porche.webp">
-                        <div class="background-of-car">
-                                <p>Porsche Tycan</p>
-                                <img src="public/img/0-100icon.svg">
-                                <p>2,6</p>
+            </a>
+            <a href="porsche_tycan">
+                <div class="car">
+                    <img src="public/img/porche.webp">
+                    <div class="background-of-car">
+                        <p>Audi RS E-tron GT</p>
+                        <img src="public/img/0-100icon.svg">
+                        <p>3,3</p>
                     </div>
+                </div>
+            </a>
+            <a href="porsche_tycan">
+                <div class="car">
+                    <img src="public/img/porche.webp">
+                    <div class="background-of-car">
+                        <p>Nissan Leaf</p>
+                        <img src="public/img/0-100icon.svg">
+                        <p>6,9</p>
                     </div>
-                </a>
-                <a href="porsche_tycan">
-                    <div class="car">
-                        <img src="public/img/porche.webp">
-                        <div class="background-of-car">
-                            <p>Audi RS E-tron GT</p>
-                            <img src="public/img/0-100icon.svg">
-                            <p>3,3</p>
-                        </div>
-                    </div>
-                </a>
-                <a href="porsche_tycan">
-                    <div class="car">
-                        <img src="public/img/porche.webp">
-                        <div class="background-of-car">
-                            <p>Nissan Leaf</p>
-                            <img src="public/img/0-100icon.svg">
-                            <p>6,9</p>
-                        </div>
-                    </div>
-                </a>
-            </div>
+                </div>
+            </a>
 
-            <div class="row-of-cars">
-                <a href="porsche_tycan">
-                    <div class="car">
-                        <img src="public/img/porche.webp">
-                        <div class="background-of-car">
-                            <p>BMW I3</p>
-                            <img src="public/img/0-100icon.svg">
-                            <p>7,1</p>
-                        </div>
+            <a href="porsche_tycan">
+                <div class="car">
+                    <img src="public/img/porche.webp">
+                    <div class="background-of-car">
+                        <p>BMW I3</p>
+                        <img src="public/img/0-100icon.svg">
+                        <p>7,1</p>
                     </div>
-                </a>
-                <a href="porsche_tycan">
-                    <div class="car">
-                        <img src="public/img/porche.webp">
-                        <div class="background-of-car">
-                            <p>Renault Zoe</p>
-                            <img src="public/img/0-100icon.svg">
-                            <p>9,5</p>
-                        </div>
+                </div>
+            </a>
+            <a href="porsche_tycan">
+                <div class="car">
+                    <img src="public/img/porche.webp">
+                    <div class="background-of-car">
+                        <p>Renault Zoe</p>
+                        <img src="public/img/0-100icon.svg">
+                        <p>9,5</p>
                     </div>
-                </a>
-                <a href="porsche_tycan">
-                    <div class="car">
-                        <img src="public/img/porche.webp">
-                        <div class="background-of-car">
-                            <p>Volkswagen ID.5</p>
-                            <img src="public/img/0-100icon.svg">
-                            <p>6,3</p>
-                        </div>
+                </div>
+            </a>
+            <a href="porsche_tycan">
+                <div class="car">
+                    <img src="public/img/porche.webp">
+                    <div class="background-of-car">
+                        <p>Volkswagen ID.5</p>
+                        <img src="public/img/0-100icon.svg">
+                        <p>6,3</p>
                     </div>
-                </a>
-            </div>
-                
-            <div class="row-of-cars">
-                <a href="porsche_tycan">
-                    <div class="car">
-                        <img src="public/img/porche.webp"><div class="background-of-car">
-                            <p>Tesla Moodel S</p>
-                            <img src="public/img/0-100icon.svg">
-                            <p>2,1</p>
-                        </div>
-                    </div>
-                </a>
-                <a href="porsche_tycan">
-                    <div class="car">
-                        <img src="public/img/porche.webp">
-                        <div class="background-of-car">
-                            <p>Mazda MX-30</p>
-                            <img src="public/img/0-100icon.svg">
-                            <p>9,7</p>
-                        </div>
-                    </div>
-                </a>
-                <a href="porsche_tycan">
-                    <div class="car">
-                        <img src="public/img/porche.webp">
-                        <div class="background-of-car">
-                            <p>Fiat 500</p>
-                            <img src="public/img/0-100icon.svg">
-                            <p>10,5</p>
-                        </div>
-                    </div>
-                </a>
-            </div>  
+                </div>
+            </a>
 
-            <div class="row-of-cars">
-                <a href="porsche_tycan">
-                    <div class="car">
-                        <img src="public/img/porche.webp">
-                        <div class="background-of-car">
-                            <p>Kia Niro</p>
-                            <img src="public/img/0-100icon.svg">
-                            <p>7,6</p>
-                        </div>
+            <a href="porsche_tycan">
+                <div class="car">
+                    <img src="public/img/porche.webp">
+                    <div class="background-of-car">
+                        <p>Tesla Moodel S</p>
+                        <img src="public/img/0-100icon.svg">
+                        <p>2,1</p>
                     </div>
-                </a>
-                <a href="porsche_tycan">
-                    <div class="car">
-                        <img src="public/img/porche.webp">
-                        <div class="background-of-car">
-                            <p>Volkswagen ID.Buzz</p>
-                            <img src="public/img/0-100icon.svg">
-                            <p>10,2</p>
-                        </div>
+                </div>
+            </a>
+            <a href="porsche_tycan">
+                <div class="car">
+                    <img src="public/img/porche.webp">
+                    <div class="background-of-car">
+                        <p>Mazda MX-30</p>
+                        <img src="public/img/0-100icon.svg">
+                        <p>9,7</p>
                     </div>
-                </a>
-                <a href="porsche_tycan">
-                    <div class="car">
-                        <img src="public/img/porche.webp">
-                        <div class="background-of-car">
-                            <p>Skoda Enyaq</p>
-                            <img src="public/img/0-100icon.svg">
-                            <p>8,6</p>
-                        </div>
+                </div>
+            </a>
+            <a href="porsche_tycan">
+                <div class="car">
+                    <img src="public/img/porche.webp">
+                    <div class="background-of-car">
+                        <p>Fiat 500</p>
+                        <img src="public/img/0-100icon.svg">
+                        <p>10,5</p>
                     </div>
-                </a>
-            </div>
-       </main>
-    </div>
+                </div>
+            </a>
+
+            <a href="porsche_tycan">
+                <div class="car">
+                    <img src="public/img/porche.webp">
+                    <div class="background-of-car">
+                        <p>Kia Niro</p>
+                        <img src="public/img/0-100icon.svg">
+                        <p>7,6</p>
+                    </div>
+                </div>
+            </a>
+            <a href="porsche_tycan">
+                <div class="car">
+                    <img src="public/img/porche.webp">
+                    <div class="background-of-car">
+                        <p>Volkswagen ID.Buzz</p>
+                        <img src="public/img/0-100icon.svg">
+                        <p>10,2</p>
+                    </div>
+                </div>
+            </a>
+            <a href="porsche_tycan">
+                <div class="car">
+                    <img src="public/img/porche.webp">
+                    <div class="background-of-car">
+                        <p>Skoda Enyaq</p>
+                        <img src="public/img/0-100icon.svg">
+                        <p>8,6</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </main>
+</div>
 </body>
