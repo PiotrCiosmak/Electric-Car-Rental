c347226b1564946ef330b2e775dba9c8788973dc
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-04 03:44:34 +0100

    email regex done

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -6,6 +6,7 @@ require_once __DIR__ . "/../models/UserData.php";
 require_once __DIR__ . "/../models/Car.php";
 require_once __DIR__ . "/../models/Rent.php";
 require_once __DIR__ . "/../repository/UserRepository.php";
+require_once __DIR__ . "/../repository/UserDataRepository.php";
 
 class SecurityController extends AppController
 {
@@ -21,17 +22,16 @@ class SecurityController extends AppController
             return $this->render('register', ['messages' => ['Wszystkie wymagane pola nie zostały uzupełnione!']]);
         }
 
-        //TODO sprawdzenie czy email nie zaczyna sie od @ i czy ma . i czy nie kończy sie na .
-        $emailRegex = preg_match('[@]', $user->getEmail());
-        if (!$emailRegex) {
+        $emailRegex = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/';
+        if (!(preg_match($emailRegex, $user->getEmail()))) {
             return $this->render('register', ['messages' => ['Niepoprawny adres email!']]);
         }
 
         //TODO sprawdzenie czy haslo ma 1 wielki, 1 mała, 1 znak specialny, 1 cyfre
-        //TODO upewnienie się ze bez logowania nie da się wejśc na inne podstrony niż register, sign in
-        if (strlen($user->getPassword()) < 8) {
-            return $this->render('register', ['messages' => ['Hasło jest zbyt krótkie!']]);
-        }
+        /*        $passwordRegex = '/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/';
+                if (!(preg_match($passwordRegex, $user->getPassword()))) {
+                    return $this->render('register', ['messages' => ['Niepoprawne hasło!!']]);
+                }*/
 
         $tmpUserRepository = new UserRepository();
 
@@ -49,24 +49,15 @@ class SecurityController extends AppController
 
     public function register_data_input_check()
     {
-        $userData = new UserData($_POST["first-name"], $_POST["last-name"], $_POST["phone-numer"], $_POST["street"], $_POST["house-number"], $_POST["apartment-number"], $_POST["post-code"], $_POST["city"]);
+        //ZAMIAST 12 === $_SESSION['user_id']
+        $userData = new UserData($_POST["first-name"], $_POST["last-name"], $_POST["phone-numer"], $_POST["street"], $_POST["house-number"], $_POST["apartment-number"], $_POST["post-code"], $_POST["city"], 1);
 
-        if (strlen($userData->getFirstName()) == 0)
-            return $this->render('register_data_input', ['messages' => ['Wprowadź imie!']]);
-        if (strlen($userData->getLastName()) == 0)
-            return $this->render('register_data_input', ['messages' => ['Wprowadź nazwisko!']]);
-        if (strlen($userData->getPhoneNumber()) == 0)
-            return $this->render('register_data_input', ['messages' => ['Wprowadź numer telefonu!']]);
-        if (strlen($userData->getStreet()) == 0)
-            return $this->render('register_data_input', ['messages' => ['Wprowadź ulicę!']]);
-        if (strlen($userData->getHouseNumber()) == 0)
-            return $this->render('register_data_input', ['messages' => ['Wprowadź numer domu!']]);
-        if (strlen($userData->getPostCode()) == 0)
-            return $this->render('register_data_input', ['messages' => ['Wprowadź kod pocztowy!']]);
-        if (strlen($userData->getTown()) == 0)
-            return $this->render('register_data_input', ['messages' => ['Wprowadź miejscowość!']]);
+        if ((strlen($userData->getFirstName()) == 0) || (strlen($userData->getLastName()) == 0) || (strlen($userData->getPhoneNumber()) == 0) || (strlen($userData->getStreet()) == 0) || (strlen($userData->getHouseNumber()) == 0) || (strlen($userData->getPostCode()) == 0) || (strlen($userData->getTown()) == 0)) {
+            return $this->render('register_data_input', ['messages' => ['Uzupełnij wszystkie pola!']]);
+        }
 
-        //Załadowanie danych do bazy danych
+        $userDataRepository = new UserDataRepository();
+        $userDataRepository->addUserData($userData);
         return $this->render('main_page');
     }
 
