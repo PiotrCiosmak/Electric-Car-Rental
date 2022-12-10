7052508795a78d4a3d1eed54d88501ff0d81d308
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-10 13:19:06 +0100

    tmp off

--- public/views/cookie.php
+++ public/views/cookie.php
@@ -1,11 +1,7 @@
 <?php
-/*if (strlen($_COOKIE["user_id"]) == 0) {
-    $url = "http://$_SERVER[HTTP_HOST]";
-    header("Location: {$url}/");
-}*/
-///*if (!isset($_COOKIE["user_id"])) {
-//    echo($_COOKIE["user_id"]);
-//    exit(1);
-//    /*    $url = "http://$_SERVER[HTTP_HOST]";
-//    header("Location: {$url}/");*/
-//*/
+if (!isset($_COOKIE["user_id"])) {
+/*    echo("nie mozna");
+    exit(1);*/
+/*    $url = "http://$_SERVER[HTTP_HOST]";
+    header("Location: {$url}/");*/
+}
