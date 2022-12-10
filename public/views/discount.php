d59b88e3f243d2f5a98110146e0ba258c346443b
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-10 15:23:31 +0100

    visual changes

--- public/views/discount.php
+++ public/views/discount.php
@@ -10,113 +10,124 @@ include('userCookie.php');
     </style>
     <script src="https://kit.fontawesome.com/8d0d2d9a42.js" crossorigin="anonymous"></script>
     <title>RABAT</title>
-    
+
 </head>
 
 <body>
-    <div class="discount-container">
-        <nav>
-            <img src="public/img/logo.svg" id="logo">
-            <ul>
-                <li>
-                    <a href="main_page" class="menu-button">GŁÓWNA</a>
-                </li>
-                <li>
-                    <a href="our_cars" class="menu-button">NASZE AUTA</a>
-                </li>
-                <li>
-                    <a href="f40" class="menu-button">F40 NA WESELE</a>
-                </li>
-                <li>
-                    <a href="faq" class="menu-button">FAQ</a>
-                </li>
-                <li>
-                    <a href="discount" class="menu-button">RABAT</a>
-                </li>
-                <li>
-                    <a href="contact" class="menu-button">KONTAKT</a>
-                </li>
-            </ul>
-            <a href="delete_cookie" class="logout-button">
-                <img src="public/img/logout.svg" id="logout">
-            </a>
-        </nav>
-        <main class="discount-main">
-            <div class="title">
-                <h2>RABAT NA NOWE AUTO</h2>
-            </div>
-            <div class="discount-text-box">
-                <p id="first-part-of-text">
-                    Nic nie kusi i nie przekonuje tak dobrze do zakupu nowego auta jak RABAT! Każdy planując zakup samochodu, chce w salonie otrzymać najatrakcyjniejszą ofertę.<br>
-                    MGP.rent wychodzi naprzeciw oczekiwaniom naszych klientów i ma do zaoferowania wypracowany przez lata RABAT FLOTOWY!<br>
-                    Jak skorzystać z naszej oferty rabatowej? To proste, przeprowadzimy Cię krok po kroku.
-                </p>
-            </div>
-            <div id="discount-all-box">
-                <div class="discount-box">
-                    <div class="discount-text">
-                        <p class="discount-title">
-                           ETAP 1
-                        </p>
-                        <p class="discount-content">
-                            Wejdź na stronę marki swojego wymarzonego samochodu, np. Mercedes-Benz, Audi, BMW, Volkswagen itd.
-                        </p>
-                    </div>
-                </div>
-                <div class="discount-box">
-                    <div class="discount-text">
-                        <p class="discount-title">
-                           ETAP 2
-                        </p>
-                        <p class="discount-content">
-                            Wybierz elektryczny model i skonfiguruj samochód w wybrany przez Ciebie sposób, podgrzewane siedzenia z masażem, sportowy wydech, wszystko jest w zasięgu Twojej ręki.
-                        </p>
-                    </div>
-                </div>
-                <div class="discount-box">
-                    <div class="discount-text">
-                        <p class="discount-title">
-                           ETAP 3
-                        </p>
-                        <p class="discount-content">
-                            Konfiguracja zakończona? Świetnie! Prześlij ją nam na adres electricCarRent@gmail. com lub podaj nam markę samochodu oraz kod konfiguracji. Jeżeli uzyskałeś wcześniej inny rabat, koniecznie nas o tym poinformuj!
-                        </p>
-                    </div>
-                </div>
-                <div class="discount-box">
-                    <div class="discount-text">
-                        <p class="discount-title">
-                           ETAP 4
-                        </p>
-                        <p class="discount-content">
-                            Electric Car Rent dzięki swojemu rabatowi na nowe auta obniży cenę twojej wymarzonej elektycznej fury!
-                        </p>
-                    </div>
-                </div>
-                <div class="discount-box">
-                    <div class="discount-text">
-                        <p class="discount-title">
-                           ETAP 5
-                        </p>
-                        <p class="discount-content">
-                            Rabat Ci odpowiada? Świetnie! W takim razie resztę spraw zostaw w naszych rękach, a my o wszystko zadbamy!
-                        </p>
-                    </div>
+<div class="discount-container">
+    <nav>
+        <img src="public/img/logo.svg" id="logo">
+        <ul>
+            <li>
+                <a href="main_page" class="menu-button">GŁÓWNA</a>
+            </li>
+            <li>
+                <a href="our_cars" class="menu-button">NASZE AUTA</a>
+            </li>
+            <li>
+                <a href="f40" class="menu-button">F40 NA WESELE</a>
+            </li>
+            <li>
+                <a href="faq" class="menu-button">FAQ</a>
+            </li>
+            <li>
+                <a href="discount" class="menu-button">RABAT</a>
+            </li>
+            <li>
+                <a href="contact" class="menu-button">KONTAKT</a>
+            </li>
+        </ul>
+        <a href="delete_cookie" class="logout-button">
+            <img src="public/img/logout.svg" id="logout">
+        </a>
+    </nav>
+    <main class="discount-main">
+        <div class="title">
+            <h2>RABAT NA NOWE AUTO</h2>
+        </div>
+        <div class="discount-text-box">
+            <p id="first-part-of-text">
+                Nic nie kusi i nie przekonuje tak dobrze do zakupu nowego auta jak RABAT! Każdy planując zakup
+                samochodu, chce w salonie otrzymać najatrakcyjniejszą ofertę.<br>
+                MGP.rent wychodzi naprzeciw oczekiwaniom naszych klientów i ma do zaoferowania wypracowany przez lata
+                RABAT FLOTOWY!<br>
+                Jak skorzystać z naszej oferty rabatowej? To proste, przeprowadzimy Cię krok po kroku.
+            </p>
+        </div>
+        <div id="discount-all-box">
+            <div class="discount-box">
+                <div class="discount-text">
+                    <p class="discount-title">
+                        ETAP 1
+                    </p>
+                    <p class="discount-content">
+                        Wejdź na stronę marki swojego wymarzonego samochodu, np. Mercedes-Benz, Audi, BMW, Volkswagen
+                        itd.
+                    </p>
                 </div>
             </div>
-            <div class="discount-text-box">
-                <p>
-                    Co Electric Car Rent otrzymuje dzięki użyczaniu swojego RABATU FLOTOWEGO? To co najważniejsze, czyli satysfakcję i zadowolenie swoich klientów! Nie pobieramy prowizji, Twoje pieniądze przechodzą prosto do salonu!<br>
-                    Nasza oferta brzmi kusząco? To nie wszystko! Możemy dodatkowo pomóc Ci w zorganizowaniu najlepszego finansowania na wymarzone auto.<br>
-                    Masz jakieś pytania? Jesteśmy do Twojej dyspozycji!
-                </p>
+            <div class="discount-box">
+                <div class="discount-text">
+                    <p class="discount-title">
+                        ETAP 2
+                    </p>
+                    <p class="discount-content">
+                        Wybierz elektryczny model i skonfiguruj samochód w wybrany przez Ciebie sposób, podgrzewane
+                        siedzenia z masażem, sportowy wydech, wszystko jest w zasięgu Twojej ręki.
+                    </p>
+                </div>
             </div>
-            <i class="fa-solid fa-phone fa-7x"></i>
-            <div class="discount-text-box">
-                <p>
-                    +48 791 371 715
-                </p>
+            <div class="discount-box">
+                <div class="discount-text">
+                    <p class="discount-title">
+                        ETAP 3
+                    </p>
+                    <p class="discount-content">
+                        Konfiguracja zakończona? Świetnie! Prześlij ją nam na adres electricCarRent@gmail. com lub podaj
+                        nam markę samochodu oraz kod konfiguracji. Jeżeli uzyskałeś wcześniej inny rabat, koniecznie nas
+                        o tym poinformuj!
+                    </p>
+                </div>
             </div>
-        </main>
-    </div>
+            <div class="discount-box">
+                <div class="discount-text">
+                    <p class="discount-title">
+                        ETAP 4
+                    </p>
+                    <p class="discount-content">
+                        Electric Car Rent dzięki swojemu rabatowi na nowe auta obniży cenę twojej wymarzonej elektycznej
+                        fury!
+                    </p>
+                </div>
+            </div>
+            <div class="discount-box">
+                <div class="discount-text">
+                    <p class="discount-title">
+                        ETAP 5
+                    </p>
+                    <p class="discount-content">
+                        Rabat Ci odpowiada? Świetnie! W takim razie resztę spraw zostaw w naszych rękach, a my o
+                        wszystko zadbamy!
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="discount-text-box">
+            <p>
+                Co Electric Car Rent otrzymuje dzięki użyczaniu swojego RABATU FLOTOWEGO? To co najważniejsze, czyli
+                satysfakcję i zadowolenie swoich klientów! Nie pobieramy prowizji, Twoje pieniądze przechodzą prosto do
+                salonu!<br>
+                Nasza oferta brzmi kusząco? To nie wszystko! Możemy dodatkowo pomóc Ci w zorganizowaniu najlepszego
+                finansowania na wymarzone auto.<br>
+                Masz jakieś pytania? Jesteśmy do Twojej dyspozycji!
+            </p>
+        </div>
+        <i class="fa-solid fa-phone fa-7x"></i>
+        <div class="discount-text-box">
+            <p>
+                +48 791 371 715
+            </p>
+        </div>
+    </main>
+</div>
 </body>
