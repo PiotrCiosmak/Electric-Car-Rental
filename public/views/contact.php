50cb403d406eb581c18f79bea02503cbefb2e549
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-10 14:02:07 +0100

    visual changes

--- public/views/contact.php
+++ public/views/contact.php
@@ -13,50 +13,50 @@ include('userCookie.php');
 </head>
 
 <body>
-    <div class="contact-container">
-       <nav>
-            <img src="public/img/logo.svg" id="logo">
-            <ul>
-                <li>
-                    <a href="main_page" class="menu-button">GŁÓWNA</a>
-                </li>
-                <li>
-                    <a href="our_cars" class="menu-button">NASZE AUTA</a>
-                </li>
-                <li>
-                    <a href="f40" class="menu-button">F40 NA WESELE</a>
-                </li>
-                <li>
-                    <a href="faq" class="menu-button">FAQ</a>
-                </li>
-                <li>
-                    <a href="discount" class="menu-button">RABAT</a>
-                </li>
-                <li>
-                    <a href="contact" class="menu-button">KONTAKT</a>
-                </li>
-            </ul>
-            <a href="sign_in" class="logout-button">
-                <img src="public/img/logout.svg" id="logout">
-            </a>
-       </nav>
-       <main class="contact-main">
-            <div class="title">
-                <h2>KONTAKT</h2>
+<div class="contact-container">
+    <nav>
+        <img src="public/img/logo.svg" id="logo">
+        <ul>
+            <li>
+                <a href="main_page" class="menu-button">GŁÓWNA</a>
+            </li>
+            <li>
+                <a href="our_cars" class="menu-button">NASZE AUTA</a>
+            </li>
+            <li>
+                <a href="f40" class="menu-button">F40 NA WESELE</a>
+            </li>
+            <li>
+                <a href="faq" class="menu-button">FAQ</a>
+            </li>
+            <li>
+                <a href="discount" class="menu-button">RABAT</a>
+            </li>
+            <li>
+                <a href="contact" class="menu-button">KONTAKT</a>
+            </li>
+        </ul>
+        <a href="sign_in" class="logout-button">
+            <img src="public/img/logout.svg" id="logout">
+        </a>
+    </nav>
+    <main class="contact-main">
+        <div class="title">
+            <h2>KONTAKT</h2>
+        </div>
+        <div id="contact-info">
+            <div id="phone-numer-box">
+                <p id="phone-number-label">Infolinia</p>
+                <p id="phone-number">+48 791 371 715</p>
             </div>
-            <div id="contact-info">
-                <div id="phone-numer-box">
-                    <p id="phone-number-label">Infolinia</p>
-                    <p id="phone-number">+48 791 371 715</p>
-                </div>
-                <div id="email-address-box">
-                    <p id="email-address-label">e-mail</p>
-                    <p id="email-address">electricCarRent@gmail.com</p>
-                </div>
+            <div id="email-address-box">
+                <p id="email-address-label">e-mail</p>
+                <p id="email-address">electricCarRent@gmail.com</p>
             </div>
-            <div id="map">
+        </div>
+        <div id="map">
             <img src="public/img/porche.webp">
-            </div>
-       </main>
-    </div>
+        </div>
+    </main>
+</div>
 </body>
