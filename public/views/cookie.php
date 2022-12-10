72487fd6d7d2e8ae76f339b772b5efc06268ff7a
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-10 12:41:38 +0100

    tmp disable

--- public/views/cookie.php
+++ public/views/cookie.php
@@ -1,5 +1,11 @@
 <?php
-if (!isset($_COOKIE["user_id"])) {
+/*if (strlen($_COOKIE["user_id"]) == 0) {
     $url = "http://$_SERVER[HTTP_HOST]";
     header("Location: {$url}/");
-}
+}*/
+///*if (!isset($_COOKIE["user_id"])) {
+//    echo($_COOKIE["user_id"]);
+//    exit(1);
+//    /*    $url = "http://$_SERVER[HTTP_HOST]";
+//    header("Location: {$url}/");*/
+//*/
