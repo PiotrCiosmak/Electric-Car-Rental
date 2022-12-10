4e6497c3893ffd3b48e333cbbe4e7e22cc059307
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-10 14:27:52 +0100

    delete_cookie added

--- index.php
+++ index.php
@@ -2,23 +2,24 @@
 
 require 'Routing.php';
 
-$path = trim($_SERVER['REQUEST_URI'],'/');
-$path = parse_url($path,PHP_URL_PATH);
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
 
-Routing::get('','DefaultController');
-Routing::get('register','DefaultController');
-Routing::get('register_check','SecurityController');
-Routing::get('sign_in','DefaultController');
-Routing::get('sign_in_check','SecurityController');
-Routing::get('booking','DefaultController');
-Routing::get('contact','DefaultController');
-Routing::get('discount','DefaultController');
-Routing::get('f40','DefaultController');
-Routing::get('faq','DefaultController');
-Routing::get('main_page','DefaultController');
-Routing::get('our_cars','DefaultController');
-Routing::get('porsche_tycan','DefaultController');
-Routing::get('register_data_input','DefaultController');
-Routing::get('register_data_input_check','SecurityController');
-Routing::get('booking_check','SecurityController');
+Routing::get('', 'DefaultController');
+Routing::get('register', 'DefaultController');
+Routing::get('register_check', 'SecurityController');
+Routing::get('sign_in', 'DefaultController');
+Routing::get('sign_in_check', 'SecurityController');
+Routing::get('booking', 'DefaultController');
+Routing::get('contact', 'DefaultController');
+Routing::get('discount', 'DefaultController');
+Routing::get('f40', 'DefaultController');
+Routing::get('faq', 'DefaultController');
+Routing::get('main_page', 'DefaultController');
+Routing::get('our_cars', 'DefaultController');
+Routing::get('porsche_tycan', 'DefaultController');
+Routing::get('register_data_input', 'DefaultController');
+Routing::get('register_data_input_check', 'SecurityController');
+Routing::get('booking_check', 'SecurityController');
+Routing::get('delete_cookie', 'CookieController');
 Routing::run($path);
