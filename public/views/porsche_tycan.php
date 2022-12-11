acf0d6c44ede68eb31eda05bebe57277760b7422
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-11 01:35:44 +0100

    todo added

--- public/views/porsche_tycan.php
+++ public/views/porsche_tycan.php
@@ -5,7 +5,7 @@ include('user_cookie.php');
 require_once __DIR__ . "/../../src/repository/CarRepository.php";
 $carRepository = new CarRepository();
 $cookie_name = "car_id";
-$cookie_value = $carRepository->getId($carName);//encrypt chyba
+$cookie_value = $carRepository->getId($carName);//TODO encrypt chyba
 setcookie($cookie_name, $cookie_value, 0, "/");
 ?>
 <!DOCTYPE html>
