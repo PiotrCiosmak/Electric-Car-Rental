e1ed0b5e281cad7481ff650cfdd5fc01059c7828
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-11 17:03:59 +0100

    function addRent implemented

--- src/repository/RentRepository.php
+++ src/repository/RentRepository.php
@@ -13,12 +13,19 @@ class RentRepository extends Repository
     public function DateIsFree(Rent $rent): bool
     {
         //TODO
+        //zwracam tblice z wszystkimi rekordmi gdzie id_car = temu z cookie
+        //do tego warunek że data poczatkowa nowa musi być wcześniej niż data końcowa
+        //do tego warunerk że data końcowa nowego musi być wcześniejsza niż data początkowa tego z bazy
         return true;
     }
 
 
     public function addRent(Rent $rent): void
     {
-        //TODO
+        $stmt = $this->database->connect()->prepare('
+            INSERT INTO rents (start_date, end_date,id_user,id_car)
+            VALUES (?,?,?,?)
+            ');
+        $stmt->execute([$rent->getBeginDate(), $rent->getEndDate(), $rent->getUserID(), $rent->getCarID()]);
     }
 }
