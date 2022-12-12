31a301056ba1a98f606b86e101faed0c1cb4d6a2
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-12 22:07:42 +0100

    account page updated

--- public/views/account.php
+++ public/views/account.php
@@ -1,13 +1,8 @@
 <?php
-$carName = "Porsche Tycan"; //Zmienna
 include('user_cookie.php');
 
-require_once __DIR__ . "/../../src/repository/CarRepository.php";
-$carRepository = new CarRepository();
-$cookie_name = "car_id";
-$cookie_value = $carRepository->getId($carName);
-setcookie($cookie_name, $cookie_value, 0, "/");
-$tmpCarRepository = new CarRepository();
+require_once __DIR__ . "/../../src/repository/UserDataRepository.php";
+$tmpUserDataRepository = new UserDataRepository();
 ?>
 <!DOCTYPE html>
 <head>
@@ -16,7 +11,7 @@ $tmpCarRepository = new CarRepository();
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap');
     </style>
     <script src="https://kit.fontawesome.com/8d0d2d9a42.js" crossorigin="anonymous"></script>
-    <title>Porsche Tycan</title>
+    <title>Konto</title>
 </head>
 
 <body>
@@ -51,7 +46,77 @@ $tmpCarRepository = new CarRepository();
         </a>
     </nav>
     <main class="account-main">
-        echo("xd");
-    </main>
+        <div class="title">
+            <h2>KONTO</h2>
+        </div>
+        <div id="account-all-columns">
+            <div class="account-col" id="account-col-1">
+                <div id="account-user-data">
+                    <h3 class="account-sub-title">Dane osobowe</h3>
+                    <p class="account-text">Imię:
+                        <?php
+                        echo($tmpUserDataRepository->getFirstName());
+                        ?>
+                    </p>
+                    <p class="account-text">Nazwisko:
+                        <?php
+                        echo($tmpUserDataRepository->getLastName());
+                        ?>
+                    </p>
+                    <p class="account-text">Numer telefonu:
+                        <?php
+                        echo($tmpUserDataRepository->getPhoneNumber());
+                        ?>
+                    </p>
+                    <p class="account-text">Ulica:
+                        <?php
+                        echo($tmpUserDataRepository->getStreet());
+                        ?>
+                    </p>
+                    <p class="account-text">Numer domu:
+                        <?php
+                        echo($tmpUserDataRepository->getHouseNumber());
+                        ?>
+                    </p>
+                    <p class="account-text">Numer mieszkania:
+                        <?php
+                        echo($tmpUserDataRepository->getApartmentNumber());
+                        ?>
+                    </p>
+                    <p class="account-text">Kod pocztowy:
+                        <?php
+                        echo($tmpUserDataRepository->getPostCode());
+                        ?>
+                    </p>
+                    <p class="account-text">Miejscowość:
+                        <?php
+                        echo($tmpUserDataRepository->getTown());
+                        ?>
+                    </p>
+                    <form action="TODO">
+                        <button id="change-user-data">Zmień dane adresowe</button>
+                    </form>
+                </div>
+                <div id="account-user-change-password">
+                    <h3 class="account-sub-title">Zmień hasło</h3>
+                    <form id="change-password">//todo ekran info ze zmienono i pozniej do account
+                        <div>
+                            <input class="input-change-password" id="input-change-password-old-password"
+                                   placeholder="stare hasło">
+                            <input class="input-change-password" id="input-change-password-new-password"
+                                   placeholder="nowe hasło">
+                            <input class="input-change-password" id="input-change-password-new-password"
+                                   placeholder="powtórz nowe hasło">
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="account-col" id="account-col-2">
+                <h3 class="account-sub-title">Twoje rezerwacje</h3>
+
+            </div>
+        </div>
+</div>
+</main>
 </div>
 </body>
