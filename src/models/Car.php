b20dd5d5c0b4723156429cf8e429a99c081323b9
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-16 19:55:47 +0100

    two var added

--- src/models/Car.php
+++ src/models/Car.php
@@ -5,10 +5,15 @@ class Car
     private $name;
     private $price;
 
-    public function __construct(string $name, float $price)
+    private $timeTo100;
+    private $imgSource;
+
+    public function __construct(string $name, float $price, float $timeTo100 = 0, string $imgSource = "")
     {
         $this->name = $name;
         $this->price = $price;
+        $this->timeTo100 = $timeTo100;
+        $this->imgSource = $imgSource;
     }
 
     public function getName(): string
@@ -29,5 +34,25 @@ class Car
     public function setPrice(float $price): void
     {
         $this->price = $price;
+    }
+
+    public function getTimeTo100(): float
+    {
+        return $this->timeTo100;
+    }
+
+    public function setTimeTo100(float $timeTo100): void
+    {
+        $this->timeTo100 = $timeTo100;
+    }
+
+    public function getImgSource(): string
+    {
+        return $this->imgSource;
+    }
+
+    public function setImgSource(string $imgSource): void
+    {
+        $this->imgSource = $imgSource;
     }
 }
