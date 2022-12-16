232d1e0810b19be682869b08974ddb53ac3d0afa
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-16 19:56:05 +0100

    empty class added

---
+++ src/controllers/CarController.php
@@ -0,0 +1,13 @@
+<?php
+
+class CarController extends AppController
+{
+    public function __construct()
+    {
+        parent::__constructor();
+    }
+    public function search()
+    {
+        //TODO
+    }
+}
