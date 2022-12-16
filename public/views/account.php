b8c086fb4a944121eb66406a2405fba20e96827a
Piotr Ciosmak <piotrciosmak2001@gmail.com>
2022-12-16 15:37:56 +0100

    main menu as a diffrent file

--- public/views/account.php
+++ public/views/account.php
@@ -16,35 +16,9 @@ $tmpUserDataRepository = new UserDataRepository();
 
 <body>
 <div class="one-car-container">
-    <nav>
-        <img src="public/img/logo.svg" id="logo">
-        <ul>
-            <li>
-                <a href="main_page" class="menu-button">GŁÓWNA</a>
-            </li>
-            <li>
-                <a href="our_cars" class="menu-button">NASZE AUTA</a>
-            </li>
-            <li>
-                <a href="f40" class="menu-button">F40 NA WESELE</a>
-            </li>
-            <li>
-                <a href="faq" class="menu-button">FAQ</a>
-            </li>
-            <li>
-                <a href="discount" id="bottom-button" class="menu-button">RABAT</a>
-            </li>
-            <li>
-                <a href="contact" id="bottom-button" class="menu-button">KONTAKT</a>
-            </li>
-            <li id="last-button">
-                <a href="account"  class="menu-button">KONTO</a>
-            </li>
-        </ul>
-        <a href="delete_cookie" class="logout-button">
-            <img src="public/img/logout.svg" id="logout">
-        </a>
-    </nav>
+    <?php
+    include('menu.php');
+    ?>
     <main class="account-main">
         <div class="title">
             <h2>KONTO</h2>
--- public/views/our_cars.php
+++ public/views/our_cars.php
@@ -15,35 +15,9 @@ include('user_cookie.php');
 
 <body>
 <div class="our-cars-container">
-    <nav>
-        <img src="public/img/logo.svg" id="logo">
-        <ul>
-            <li>
-                <a href="main_page" class="menu-button">GŁÓWNA</a>
-            </li>
-            <li>
-                <a href="our_cars" class="menu-button">NASZE AUTA</a>
-            </li>
-            <li>
-                <a href="f40" class="menu-button">F40 NA WESELE</a>
-            </li>
-            <li>
-                <a href="faq" class="menu-button">FAQ</a>
-            </li>
-            <li>
-                <a href="discount" id="bottom-button" class="menu-button">RABAT</a>
-            </li>
-            <li>
-                <a href="contact" id="bottom-button" class="menu-button">KONTAKT</a>
-            </li>
-            <li id="last-button">
-                <a href="account" class="menu-button">KONTO</a>
-            </li>
-        </ul>
-        <a href="delete_cookie" class="logout-button">
-            <img src="public/img/logout.svg" id="logout">
-        </a>
-    </nav>
+    <?php
+    include('menu.php');
+    ?>
     <main class="our-cars-main">
         <div class="title">
             <h2>NASZE AUTA</h2>
